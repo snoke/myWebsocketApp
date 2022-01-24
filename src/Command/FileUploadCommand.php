ce9fb1387af49c194473ae4519c02e5ebc752dff
unknown <stowwel@gmail.com>
2022-01-24 11:31:03 +0100

    adding file upload support

---
+++ src/Command/FileUploadCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\File;
+use App\Entity\User;
+
+#[AsCommand(
+    name: 'app:file:upload',
+    description: 'Uploads a file',
+)]
+class FileUploadCommand extends Command
+{
+    public function __construct(EntityManagerInterface $em) {
+        parent::__construct();
+        $this->em = $em;
+
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('userId', InputArgument::REQUIRED, 'Argument description')
+            ->addArgument('filename', InputArgument::REQUIRED, 'Argument description')
+            ->addArgument('content', InputArgument::REQUIRED, 'Argument description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        
+        $file = new File();
+
+        $file->setUser($this->em->getRepository(User::class)->findOneBy(['id'=> $input->getArgument('userId')]));
+        $file->setFilename($input->getArgument('filename'));
+        $file->setContent($input->getArgument('content'));
+
+        $this->em->persist($file);
+        $this->em->flush();
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Entity/File.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FileRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FileRepository::class)]
+class File
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    private $user;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $filename;
+
+    #[ORM\Column(type: 'text')]
+    private $content;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): self
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
