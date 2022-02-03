09ce6ec08214a11001d0ad298632373003e8783c
unknown <stowwel@gmail.com>
2022-02-03 21:12:59 +0100

    removing obsolete import

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -6,8 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use App\Api\ChatApi;
-
 class AppController extends AbstractController
 {
     /**
